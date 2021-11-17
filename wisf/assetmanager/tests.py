6aed715fb3a1dd55dd81990d35fc1d3802d91331
Arivald8 <arivald88@gmail.com>
2021-11-17 20:18:03 +0000

    Added unittest to tests.py. Created a NewVisitorTest class and provided it with setUp and terDown methods. A method to navigate the home page and assert page title also added.

--- wisf/assetmanager/tests.py
+++ wisf/assetmanager/tests.py
@@ -1,8 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
 
-assert "The install worked" in browser.title
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+        return super().setUp()
 
+    def tearDown(self) -> None:
+        return super().tearDown()
 
+    def test_can_navigate_to_home_page(self):
+        # User navigates to the to the home page of the web app
+        self.browser.get("http://localhost:8000")
+        # User notices the page title
+        assert "WISF Asset Manager" in self.browser.title
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
