39956c706bba8c4105f6835d06d71f2c00ec52e5
Arivald8 <arivald88@gmail.com>
2021-11-18 11:45:28 +0000

    Added Firebase Authentication to the project. Dummy login and signup html files added to the templates. Created respective dummy views to test the Firebase Auth. Added functional tests to test sign up template navigation. Both tests passed.

--- wisf/assetmanager/functional_tests.py
+++ wisf/assetmanager/functional_tests.py
@@ -11,10 +11,17 @@ class NewVisitorTest(unittest.TestCase):
         return super().tearDown()
 
     def test_can_navigate_to_home_page(self):
-        # User navigates to the to the home page of the web app
+        # User navigates to the home page of the web app
         self.browser.get("http://localhost:8000")
         # User notices the page title
-        self.assertIn("WISF Asset Manager", self.browser.title)
+        self.assertIn("Sign In", self.browser.title)
+
+    def test_can_sign_up_for_an_account(self):
+        # User navigates to the sign up url
+        self.browser.get("http://localhost:8000/signUp/")
+        # User notices the Sign Up form
+        self.browser.find_element_by_id("signup_form")
+
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
--- wisf/assetmanager/views.py
+++ wisf/assetmanager/views.py
@@ -1,4 +1,63 @@
 from django.shortcuts import render
+from decouple import config as cfg
+import pyrebase
+
+config={
+    "apiKey": cfg('FIREBASE_apiKey'),
+    "authDomain": cfg('FIREBASE_authDomain'),
+    "projectId": cfg('FIREBASE_projectId'),
+    "storageBucket": cfg('FIREBASE_storageBucket'),
+    "messagingSenderId": cfg('FIREBASE_messagingSenderId'),
+    "appId": cfg('FIREBASE_appId'),
+    "databaseURL": "NONE",
+    "measurementId": cfg('FIREBASE_measurementId')
+}
+
+# Initialising the above config
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+
+def sign_in(request):
+    return render(request, "login.html")
+
+def post_sign_in(request):
+    email = request.POST.get('email')
+    password = request.POST.get('pass')
+
+    try:
+        # If there is no error, then sign in 
+        # the user with provided credentials
+        user = authe.sign_in_with_email_and_password(email, password)
+    except:
+        message = "Invalid Credentials..."
+        return render(request, "login.html", {"message": message})
+
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
+    return render(request, "index.html", {"email": email})
+
+def logout(request):
+    try:
+        del request.session['uid']
+    except:
+        pass
+    return render(request, "login.html")
+
+def sign_up(request):
+    return render(request, "registration.html")
+
+def post_sign_up(request):
+    email = request.POST.get('email')
+    password = request.POST.get('pass')
+    name = request.POST.get('name')
+    try:
+        user = authe.create_user_with_email_and_password(email, password)
+        uid = user['localId']
+        idtoken = request.session['uid']
+    except:
+        return render(request, "registration.html")
+    return render(request, "login.html")
+
 
 def home_page(request):
     return render(request, 'index.html')
