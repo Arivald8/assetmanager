b143f640c4e0ec0426773114ed84793790b572f2
Arivald8 <Arivald88@gmail.com>
2021-11-21 18:03:45 +0000

    Created an Authenticator subsystem to abstract away the authentication within views. Instead of defining the firebase credentials and authenticating users inside of a view, the request is passed to an Authenticator that deals with it. Adjusted the views as appropriate. Only user sign in, sign out and sign up are currently supported.

--- wisf/assetmanager/views.py
+++ wisf/assetmanager/views.py
@@ -1,74 +1,28 @@
 from django.shortcuts import render
 from decouple import config as cfg
 
-import firebase_admin
 from firebase_admin import auth
-import pyrebase
 
-cred = firebase_admin.credentials.Certificate(f"{cfg('GOOGLE_CREDENTIALS')}")
-default_app = firebase_admin.initialize_app(cred)
-
-config={
-    "apiKey": cfg('FIREBASE_apiKey'),
-    "authDomain": cfg('FIREBASE_authDomain'),
-    "projectId": cfg('FIREBASE_projectId'),
-    "storageBucket": cfg('FIREBASE_storageBucket'),
-    "messagingSenderId": cfg('FIREBASE_messagingSenderId'),
-    "appId": cfg('FIREBASE_appId'),
-    "databaseURL": "NONE",
-    "measurementId": cfg('FIREBASE_measurementId')
-}
-
-# Initialising the above config
-firebase = pyrebase.initialize_app(config)
-authe = firebase.auth()
+from assetmanager.authenticator import Authenticator
 
 
 def sign_in(request):
     return render(request, "login.html")
 
 def post_sign_in(request):
-    email = request.POST.get('email')
-    password = request.POST.get('pass')
-
-    try:
-        # If there is no error, then sign in 
-        # the user with provided credentials
-        user = authe.sign_in_with_email_and_password(email, password)
-        pass
-    except:
-        message = "Invalid Credentials..."
-        return render(request, "login.html", {"message": message})
-
-    request.session['idToken'] = str(user['idToken'])
-    request.session['uid'] = str(user['localId'])
-    request_keys = [_ for _ in request.session.items()]
-    return render(request, "index.html", {"request_keys": request_keys})
+    validation = Authenticator().user_sign_in(request=request)
+    return render(validation[0], validation[1], validation[2])
 
 def logout(request):
-    try:
-        del request.session['idToken']
-    except:
-        pass
-    return render(request, "index.html")
+    validation = Authenticator().user_logout(request)
+    return render(validation[0], validation[1])
 
 def sign_up(request):
     return render(request, "registration.html")
 
 def post_sign_up(request):
-    email = request.POST.get('email')
-    password = request.POST.get('pass')
-
-    try:
-        user = auth.create_user(email=email, password=password)
-        print("DEBUG USER CREATION---------------------------")
-        print(user)
-        uid = user['localId']
-        user_idtoken = request.session['uid']
-        #custom_token = auth.create_custom_token(uid, {"admin": True})
-    except:
-        return render(request, "registration.html")
-    return render(request, "login.html")
+    validation = Authenticator().user_sign_up(request)
+    return render(validation[0], validation[1])
 
 
 def home_page(request):
@@ -97,9 +51,12 @@ def add_admin_claim(request):
 
 def show_user_claims(request):
     claims = auth.verify_id_token(request.session['idToken'])
-    if claims['admin'] is True:
-        print("It Works")
-        print(auth.get_user(request.session['uid']).custom_claims.get('admin'))
-    else:
-        print("It sucks")
+    try:
+        if claims['admin'] is True:
+            print("User is an admin")
+            print(auth.get_user(request.session['uid']).custom_claims.get('admin'))
+        else:
+            print("Not an admin")
+    except KeyError:
+        print("Not an admin")
     return render(request, "index.html")
