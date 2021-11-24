24c33334dfe78ad5131a5884e66ed4766e3ac601
Arivald8 <Arivald88@gmail.com>
2021-11-24 11:13:47 +0000

    Added more tests and removed redundant tests. No test failures. 1 error.

--- wisf/assetmanager/tests.py
+++ wisf/assetmanager/tests.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.sessions import middleware
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.urls import resolve
 from django.test import TestCase
+from django.test.client import RequestFactory
+
 from decouple import config as cfg
+
+from firebase_admin import auth as fb_auth
+
 from assetmanager.views import home_page
 from assetmanager.authenticator import Authenticator
 
@@ -12,50 +20,44 @@ class HomePageTest(TestCase):
 
 
 class AuthenticatorTest(TestCase):
-    def test_object_initialization(self):
-        auth = Authenticator()
-        self.assertEqual(auth.user_obj, None)
-        #self.assertEqual(auth.)
-        # Write rest of the tests here
-
-    
-    def test_string_representation(self):
-        # User object has been initialized
-        # but the user is not registered
-        self.assertEqual(
-            "Authenticator objects contains an unregistered user.",
-            Authenticator().__str__()
-        )
-        # User object has not been initialized
-        self.assertEqual(
-            "None",
-            Authenticator().__str__()
-        )
-        # User object has been initialized
-        # and user is registered
-        self.assertEqual(
-            "Authenticator contains:",
-            Authenticator().__str__()
-        )
-
-
-    def test_repr_representation(self):
-        # User object has been initialized
-        self.assertIn(
-            "Authenticator contains:",
-            Authenticator().__repr__()
-        )
-        # User object has not been initialized
-        self.assertIn(
-            "Authenticator object contains",
-            Authenticator().__repr__()
-        )
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.auth = Authenticator()
 
     
     def test_access_denied_method(self):
         self.assertListEqual(
             ["index.html", {"unauthorized": "Access Denied"}],
-            Authenticator().access_denied()
+            self.auth.access_denied()
         )
 
+
+    def test_user_can_sign_in(self):
+        request = self.factory.post('signin/', {'email': cfg('TEST_EMAIL'), 'pass': cfg('TEST_PASSWORD')})
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
         
+        sign_in_request = Authenticator().user_sign_in(request)
+        self.assertTrue(sign_in_request[2]['request_keys'])
+
+    
+    def test_if_user_has_admin_claims(self):
+        request = self.factory.get('addclaims/')
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
+        request.session['idToken'] = self.user['idToken']
+
+        claims = fb_auth.verify_id_token(request.session['idToken'])
+        print(claims['admin'])
+
+        # User with admin claims
+        self.assertTrue(Authenticator().is_admin(request))
+
+        request.session['idToken'] = "None"
+
+        # Anonymous user with no claims
+        self.assertFalse(Authenticator().is_admin(request))
+        
+
