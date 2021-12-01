8ebf890ea7be274140475e54d77afbcb8b92bee6
Arivald8 <Arivald88@gmail.com>
2021-12-01 19:29:16 +0000

    Testing React and Django integration due to CSRF issues.

--- wisf/front/my-app/src/components/Signin.jsx
+++ wisf/front/my-app/src/components/Signin.jsx
@@ -1,12 +1,34 @@
 import "./Signin.css";
 
 export default function Signin(props){
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        let credentials = {
+            "email": e.target.email.value,
+            "password": e.target.password.value
+        }
+        sendSubmit(credentials)
+    }
+
+    let sendSubmit = (cred) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(cred)
+        };
+
+        fetch('http://127.0.0.1:8000/postsignin/', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data))
+
+    }
+
     return(
         <div className="sign_in_form_div">
             <p className="sign_in_title">Sign In</p>
-            <form className="sign_in_form" action="/">
-                <input className="sign_in_email" type="email" placeholder="Example@email.com"></input>
-                <input className="sign_in_password" type="password" placeholder="Password"></input>
+            <form className="sign_in_form" onSubmit={handleSubmit}>
+                <input className="sign_in_email" type="email" name="email" placeholder="Example@email.com"></input>
+                <input className="sign_in_password" type="password" name="password" placeholder="Password"></input>
                 <button className="sing_in_submit_btn" type="submit">Sign In</button>
                 <p className="sign_in_forgot_pass"><a href='#'>Forgot Password?</a></p>
             </form>
--- wisf/wisf/settings.py
+++ wisf/wisf/settings.py
@@ -15,7 +15,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = False
 
 
 # Application definition
@@ -28,9 +31,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'assetmanager',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +123,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
