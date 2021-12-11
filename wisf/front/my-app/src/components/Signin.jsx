d0af931635de478afbdab6cd2c8aed0ff49999b4
Arivald8 <arivald88@gmail.com>
2021-12-11 23:03:20 +0000

    Add,Change: Adding an ApiCaller component, repsonsible for API calls to the back end server. Changed the Signin component to account for the new ApiCaller.

--- wisf/front/my-app/src/components/Signin.jsx
+++ wisf/front/my-app/src/components/Signin.jsx
@@ -1,46 +1,17 @@
 import "./Signin.css";
 import { useNavigate } from 'react-router-dom';
+import { csrftoken, makeRequest } from './ApiCaller';
 
 export default function Signin(){
     const navigate = useNavigate();
 
-    function getCookie(name){
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== ''){
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++){
-                const cookie = cookies[i].trim();
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')){
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-
-    const csrftoken = getCookie('csrftoken');
-
-    let handleSubmit = (e) => {
+    function handleSubmit(e){
         e.preventDefault();
         let formData = new FormData();
         formData.append('email', e.target.email.value)
         formData.append('pass', e.target.password.value)
-        sendSubmit(formData, e)
-    }
-
-    let sendSubmit = (cred, e) => {
-        const request = new Request(
-            'http://127.0.0.1:8000/postsignin/',
-            {
-                headers: {'X-CSRFToken': csrftoken},
-                method: 'POST',
-                mode: 'cors',
-                credentials: 'include',
-                body: cred
-            }
-        );
+        let request = makeRequest(formData, 'postsignin')
+    
 
         fetch(request).then(function(response){
             return response.json(); 
@@ -55,9 +26,7 @@ export default function Signin(){
 
             }
         });
-
     }
-
     return(
         <div className="sign_in_form_div">
             <p className="sign_in_title">Sign In</p>
