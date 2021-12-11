45d544ab204244fdc0a2b774a7785bfdb1aaca1d
Arivald8 <arivald88@gmail.com>
2021-12-11 21:32:20 +0000

    Change: Changing the behaviour of the sidenav

--- wisf/front/my-app/src/App.js
+++ wisf/front/my-app/src/App.js
@@ -5,8 +5,6 @@ export default function App(){
     let logged_in = false
     
     if (localStorage['user_email']){logged_in = true}
-    console.log(localStorage)
-    console.log(logged_in)
 
     return(
         <div className="app_div">
