640f431ee6bb22e9ab2e6f90dbe66cad44b843e7
Arivald8 <arivald88@gmail.com>
2021-12-17 18:51:55 +0000

    Created a simple table to display the API data. Populated with dummy hardcoded data.

--- wisf/front/my-app/src/components/AssetManager.jsx
+++ wisf/front/my-app/src/components/AssetManager.jsx
@@ -4,13 +4,15 @@ import fetchAssets from './AssetView';
 export default function AssetManager(){
     let assets = fetchAssets()
 
-    console.log("debug assetmanager")
-    console.log(assets)
-    console.log("debug end assetmanager")
-
     return(
         <div className="asset-manager-div">
             <div className="manager-body">
+                <div className="search-bar">
+                    <form className="search-bar-form">
+                        <input type="text" className="search-input"/>
+                        <button className="search-submit" type="submit">Search</button>
+                    </form>
+                </div>
                 <table className="asset-table">
                     <thead className="table-head">
                         <tr className="table-row">
@@ -157,37 +159,12 @@ export default function AssetManager(){
                         <td className="table-data">adbj2a</td>
                         <td className="table-data">khs</td>
                         <td className="table-data">very good</td>
-                    </tbody>
-                    <tbody className="table-body">
-                        <td className="table-data">KHS-T-125</td>
-                        <td className="table-data">Laptop</td>
-                        <td className="table-data">123456</td>
-                        <td className="table-data">abc123</td>
-                        <td className="table-data">g10</td>
-                        <td className="table-data">KHS IT office</td>
-                        <td className="table-data">123.456</td>
-                        <td className="table-data">adbj2a</td>
-                        <td className="table-data">khs</td>
-                        <td className="table-data">very good</td>
-                    </tbody>
-                    <tbody className="table-body">
-                        <td className="table-data">KHS-T-125</td>
-                        <td className="table-data">Laptop</td>
-                        <td className="table-data">123456</td>
-                        <td className="table-data">abc123</td>
-                        <td className="table-data">g10</td>
-                        <td className="table-data">KHS IT office</td>
-                        <td className="table-data">123.456</td>
-                        <td className="table-data">adbj2a</td>
-                        <td className="table-data">khs</td>
-                        <td className="table-data">very good</td>
-                    </tbody>
-                    
+                    </tbody>     
                 </table>
             </div>
 
             <div className="bottom-controls">
-                <p>Helloooooooooooooooooooooooooooooooooooooooooooooooooooo</p>
+                <button className="add-asset-btn">Add</button>
             </div>
         </div>
     )
