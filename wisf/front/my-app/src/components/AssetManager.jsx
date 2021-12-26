d61a01420411cf9cbda6810fe1430af7ebadc625
Arivald8 <arivald88@gmail.com>
2021-12-26 16:57:43 +0000

    Saving state of CSS

--- wisf/front/my-app/src/components/AssetManager.jsx
+++ wisf/front/my-app/src/components/AssetManager.jsx
@@ -1,23 +1,22 @@
 import "./AssetManager.css";
-import fetchAssets from './AssetView';
+import FetchAssets from './AssetView';
 import { useState, useEffect, setState } from "react";
 
+
 export default function AssetManager(){
-    const [assetstate, setAssetstate] = useState(0);
-    useEffect(() => {
-        let isMounted = true;
-        const resp = fetchAssets();
-        resp.then(function(data){
-            if (isMounted) setAssetstate(data);
-        })
-        return () => {isMounted = false};
-    }, []);
-
-    console.log("ASADASD")
-    console.log(assetstate.results)
-
     return(
         <div className="asset-manager-div">
+            <div className="search-bar">
+                <form className="search-bar-form">
+                    <input type="text" className="search-input"/>
+                    <button className="search-submit" type="submit">Search</button>
+                </form>
+            </div>
+            <div className="manager-body">
+                <FetchAssets />
+            </div>
+
+            {/*
             <div className="manager-body">
                 <div className="search-bar">
                     <form className="search-bar-form">
@@ -25,44 +24,14 @@ export default function AssetManager(){
                         <button className="search-submit" type="submit">Search</button>
                     </form>
                 </div>
-                <table className="asset-table">
-                    <thead className="table-head">
-                        <tr className="table-row">
-                            <th className="table-header">Device Name</th>
-                            <th className="table-header">Device Type</th>
-                            <th className="table-header">Device Asset</th>
-                            <th className="table-header">Device Serial</th>
-                            <th className="table-header">Device Model</th>
-                            <th className="table-header">Device Location</th>
-                            <th className="table-header">Device IP</th>
-                            <th className="table-header">Device Mac</th>
-                            <th className="table-header">Device School</th>
-                            <th className="table-header">Device Notes</th>
-                        </tr>
-                        {assetstate.results.map((element) => {
-                            return (
-                                <tdbody className="table-body">
-                                    <td className="table-data">{element.device_name}</td>
-                                    <td className="table-data">{element.device_type}</td>
-                                    <td className="table-data">{element.device_asset}</td>
-                                    <td className="table-data">{element.device_serial}</td>
-                                    <td className="table-data">{element.device_model}</td>
-                                    <td className="table-data">{element.device_location}</td>
-                                    <td className="table-data">{element.device_ip}</td>
-                                    <td className="table-data">{element.device_mac}</td>
-                                    <td className="table-data">{element.device_school}</td>
-                                    <td className="table-data">{element.device_notes}</td>
-                                </tdbody>
-                            )
-                        })}
-                    </thead>
-
-                </table>
+                <FetchAssets />
             </div>
 
             <div className="bottom-controls">
                 <button className="add-asset-btn">Add</button>
             </div>
+            */}
+
         </div>
     )
 }
--- wisf/front/my-app/src/components/AssetView.jsx
+++ wisf/front/my-app/src/components/AssetView.jsx
@@ -1,17 +1,95 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { makeRequest } from './ApiCaller';
 
-export default function fetchAssets(){
+export default function FetchAssets(){
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
     let request = makeRequest(null, 'manager/view-assets', 'GET')
 
-    return fetch(request).then(function(response){
-        return response.json();
-    }).then(function(data){
-        console.log("front debug")
-        console.log(data.count)
-        console.log(data.previous)
-        console.log(data.next)
-        console.log(data.results)
-        console.log('front debug end')
-        return data
-    })
+    useEffect(() => {
+        fetch(request)
+            .then(res => res.json())
+            .then(
+                (paginator_obj) => {
+                    console.log("debug here")
+                    console.log(paginator_obj)
+                    console.log("debug end here")
+                    setIsLoaded(true);
+                    setItems(paginator_obj);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className='asset-table-div'>
+                <table className='asset-table'>
+                    <thead className='table-head'>
+                        <tr className='table-row'>
+                            <th className='table-data'>Name</th>
+                            <th className='table-data'>Type</th>
+                            <th className='table-data'>Asset</th>
+                            <th className='table-data'>Serial Number</th>
+                            <th className='table-data'>Model Number</th>
+                            <th className='table-data'>IP</th>
+                            <th className='table-data'>Mac</th>
+                            <th className='table-data'>Location</th>
+                            <th className='table-data'>School</th>
+                            <th className='table-data'>Notes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {items.results?.map(item => (
+                        <tr className='table-row'>
+                            <td className='table-data'>
+                                {item.device_name}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_type}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_asset}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_serial}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_model}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_ip}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_mac}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_location}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_school}
+                            </td>
+                            <td className='table-data'>
+                                {item.device_notes}
+                            </td>
+                        </tr>
+                    ))}
+
+                    </tbody>
+                </table>
+
+            </div>
+        );
+    }
 }
+
